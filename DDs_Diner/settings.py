0078191ea5302574c06c01ccdf27785b6a2008fe
Beth Allaway <beth_allaway@yahoo.com>
2023-01-21 13:40:46 +0000

    set up database and install requirements

--- DDs_Diner/settings.py
+++ DDs_Diner/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
-
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -85,11 +84,15 @@ WSGI_APPLICATION = 'DDs_Diner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
