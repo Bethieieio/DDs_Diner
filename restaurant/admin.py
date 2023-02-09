f1af874320e0e07e6d9cdb10a116815ee0fc6eac
Beth Allaway <beth_allaway@yahoo.com>
2023-02-09 21:41:37 +0000

    add admin config for reviews and bookings

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import CreateReviews, BookingModel
 
-# Register your models here.
+@admin.register(CreateReviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ('review', 'creation_date')
+    search_fields = ['review']
+    prepopulated_fields = {}
+    list_filter = ('creation_date',)
+    #summernote_fields = ('review')
+
+
+@admin.register(BookingModel)
+class BookingsAdmin(admin.ModelAdmin):
+    list_display = ('date', 'time', 'heads', 'allergies')
+    prepopulated_fields = {}
+    list_filter = ('date', 'time',)
+
