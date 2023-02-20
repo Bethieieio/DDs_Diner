a2e1d946f85316ff3448a1f3becce6a93a901206
Beth Allaway <beth_allaway@yahoo.com>
2023-02-20 22:12:40 +0000

    allauth fixes for login

--- DDs_Diner/user-adapter.py
+++ DDs_Diner/user-adapter.py
@@ -3,16 +3,18 @@ from allauth.account.adapter import DefaultAccountAdapter
 
 class MyAccountAdapter(DefaultAccountAdapter):
     def save_user(self, request, user, form, commit=True):
-        from allauth.account.utils import user_email, user_field
+        from allauth.account.utils import user_email, user_field, user_username
         data = form.cleaned_data
         first_name = data.get('first_name')
         last_name = data.get('last_name')
         email = data.get('email')
+        user_username(user, email)
         user_email(user, email)
         if first_name:
             user_field(user, 'first_name', first_name)
         if last_name:
             user_field(user, 'last_name', last_name)
+        print(data)
         if 'password1' in data:
             user.set_password(data['password1'])
         else:
@@ -20,9 +22,4 @@ class MyAccountAdapter(DefaultAccountAdapter):
         self.populate_username(request, user)
         if commit:
             user.save()
-        return user
-
-    def populate_user(self, request, sociallogin, data):
-        user = super().populate_user(request, sociallogin, data)
-        user.username = user.email
         return user
